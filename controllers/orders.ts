175fcc75f6b3e0865071672a4255e675aacaba4c
Networkz <alex.cinco99@gmail.com>
2022-03-04 14:28:33 +0100

    Add: orders controller

--- controllers/orders.ts
+++ controllers/orders.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import Order from "../models/order";
-import User from "../models/user";
 
 /*Obtain multiple order
 async function to query all orders since method findall() return a promise so async function is needed*/
@@ -9,7 +8,7 @@ export const getOrders = async( req: Request, res: Response ) => {
   const orders = await Order.findAll(); //query all
 
   res.json({orders,
-  msg: "Hola funciono"});
+  msg: "working"});
 }
 //Obtain order
 export const getOrder = async( req: Request, res: Response ) => {
@@ -30,29 +29,64 @@ export const getOrder = async( req: Request, res: Response ) => {
   res.json(order)
 }
 //Create order
+//full_name + email + password + country ARE NOT NULL SO YOU NEED TO SEND THAT IN POSTMAN IN ORDER TO ADD A NEW order
 export const postOrder = async( req: Request, res: Response ) => {
 
-  const { body } = req;
+const { body } = req;
 
+  try {
+//Create order and insert body from the json request
+    const order = Order.build(body);
+    await order.save();
+
+    res.json( order );
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      msg: 'Talk to the administrator'
+    }) 
+  }
 }
 //Update order
-export const putOrder = ( req: Request, res: Response ) => {
+export const putOrder = async( req: Request, res: Response ) => {
 
   const { id } = req.params;
   const { body } = req;
 
-  res.json({
-    msg: 'postorder',
-    body
-  })
+  try {
+    
+    const order  = await Order.findByPk(id);
+    if(!order) {
+      return res.status(404).json({
+        msg: `order with id ${id} doesn't exist`
+      })
+    }
+//Update and return order updated
+    await order.update( body );
+    
+    res.json( order );
+    
+      } catch (error) {
+        console.log(error)
+        res.status(500).json({
+          msg: 'Talk to the administrator'
+        }) 
+      }
 }
 //Delete order
-export const deleteOrder = ( req: Request, res: Response ) => {
+export const deleteOrder = async( req: Request, res: Response ) => {
 
   const { id } = req.params;
 
-  res.json({
-    msg: 'deleteorder',
-    id
-  })
+  const order  = await Order.findByPk(id);
+    if(!order) {
+      return res.status(404).json({
+        msg: `order with id ${id} doesn't exist`
+      })
+    }
+//Physical delete from database -> delete entire row from database
+  await order.destroy()
+
+  res.json(order)
 }
